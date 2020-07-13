c0b6333b3faad7505bb1f9fcc119a57a8ecf8a7b
shenayyilmaz <shenay_yilmaz@hotmail.com>
2020-07-13 16:21:37 +0100

    fileds createdAt not to show

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,13 @@ const {
   createTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
 } = require('./../controllers/tourController');
 // 3) ROUTES
 const router = express.Router();
 
 //router.param('id', checkId);
-
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
