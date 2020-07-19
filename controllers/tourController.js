940470334270d04589e726e5e0ee26094597f782
shenayyilmaz <shenay_yilmaz@hotmail.com>
2020-07-19 12:09:44 +0100

    apiFeatures (filters ,sort,fields,paginate)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,67 +12,24 @@ exports.aliasTopTours = (req, res, next) => {
 // 2) ROUTE HANDELERS FUNCTIONS
 exports.getAllTours = async (req, res) => {
   try {
-    //BUILD QUERY
-    // 1 A) Filtering
-
-    let queryObj = { ...req.query };
-    const excludedFields = ['fields', 'page', 'sort', 'limit'];
-
-    excludedFields.forEach((el) => {
-      delete queryObj[el];
-    });
-
-    // 2 B) Advance Filtering
-    let queryStr = JSON.stringify(queryObj);
-    //() found
-    //b exactly gte or gt ..
-    // g can found many times not first one and stop
-    queryStr = queryStr.replace(
-      /\b(gte| gt | lte | lt )\b/g,
-      (match) => `$${match}`
-    );
-
-    const query = Tour.find(JSON.parse(queryStr));
-
-    // 2) SORTING
     //query.sort('difficulty price')  once difficulty gore siraliyo ayni varsa ornek easy easy ikitane bunlarida kendi arasinda price gore olucak sekilde siraliyo
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query.sort(sortBy);
-    } else {
-      query.sort('-createdAt');
-    }
 
     // 3) Field limiting
     // query.select(name difficulty price)  gibi
-    if (req.query.fields) {
-      let fields = req.query.fields.split(',').join(' ');
-      query.select(fields);
-    } else {
-      query.select('-__v');
-    }
 
     // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 10;
-    const skip = (page - 1) * limit;
-    query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page not exist');
-    }
 
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    console.log('test1', features.query);
+    const tours = await features.query;
     //query.sort().select().skip().limit()
 
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    //const tours = await Tour.find();
 
     // SEND RESPONSE
     res.status(200).json({
